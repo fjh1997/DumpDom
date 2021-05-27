3b15f999853cb8388969ed325b113cf047aeafe2
fjh1997 <fjh1997@fjh1997s-MacBook-Pro.local>
2021-05-27 20:17:39 +0800

    oneclick

--- background.js
+++ background.js
@@ -1,6 +1,67 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log('begin!');
 });
+chrome.action.onClicked.addListener(function(tab) {
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: saveDom,
+  });
+  
+  
+  });
+// The body of this function will be executed as a content script inside the
+// current page
+function saveDom() {
+ 
+  filename = "dom";
+  var html = '',
+    node = document.firstChild
+  while (node) {
+    switch (node.nodeType) {
+      case Node.ELEMENT_NODE:
+        html += node.outerHTML
+        break
+      case Node.TEXT_NODE:
+        html += node.nodeValue
+        break
+      case Node.CDATA_SECTION_NODE:
+        html += '<![CDATA[' + node.nodeValue + ']]>'
+        break
+      case Node.COMMENT_NODE:
+        html += '<!--' + node.nodeValue + '-->'
+        break
+      case Node.DOCUMENT_TYPE_NODE:
+        // (X)HTML documents are identified by public identifiers
+        html +=
+          '<!DOCTYPE ' +
+          node.name +
+          (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') +
+          (!node.publicId && node.systemId ? ' SYSTEM' : '') +
+          (node.systemId ? ' "' + node.systemId + '"' : '') +
+          '>\n'
+        break
+    }
+    node = node.nextSibling
+  }
+  
+  
+  var file = new Blob([html], {
+    type: 'text/html'
+  });
+  if (window.navigator.msSaveOrOpenBlob) // IE10+
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  else { // Others
+    var a = document.createElement("a"),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+
+}
